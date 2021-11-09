dd4505d2cc681b4432eb5d51223f04b4c3b575c0
Amin Sharifi <bigm00bnd@gmail.com>
2021-11-09 11:42:38 +0330

    make_save_file_path function imported

--- face_manipulator/utils/__init__.py
+++ face_manipulator/utils/__init__.py
@@ -1,5 +1,5 @@
 from .image import *
-from .path import is_file_ext_valid, is_path_valid, validate_path
+from .path import is_file_ext_valid, is_path_valid, validate_path, make_save_file_path
 from .face_utils import *
 from .manipulator import *
 from .command_line import parse_args
